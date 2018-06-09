89dd95e8c59363b2e5a284fc9b4722454e7b9c96
David Holland <s00731717@acad.tri-c.edu>
2018-06-09 17:53:41 -0400

    Added Week 3 Math csproj.

---
+++ IT1050ConsoleApps/Math/MyMath.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math
+{
+    public class MyMath
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Factorial(int n)
+        {
+            int answer = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                answer *= i;
+            }
+            return answer;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Min(int a, int b)
+        {
+            return System.Math.Min(a, b); 
+
+            //If you wanted the work shown, here it is:
+            //int answer = a;
+            //if (a > b)
+            //{
+            //    answer = b;
+            //}
+            //return answer;
+        }
+    }
+}
---
+++ IT1050ConsoleApps/Math/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+            Console.WriteLine(math.Add(9, 19));
+            Console.WriteLine(math.Factorial(9));
+            Console.WriteLine(math.Min(20, 21));
+            Console.WriteLine(math.Multiply(19, 2));
+            Console.Read();
+        }
+    }
+}
